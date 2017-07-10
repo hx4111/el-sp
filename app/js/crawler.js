e972822c464275df1ea2533d4dca49c45163c70f
haoxu <haoxu@xunlei.com>
2017-07-10 18:59:06 +0800

    commit

--- app/js/crawler.js
+++ app/js/crawler.js
@@ -4,60 +4,92 @@ import request from 'request'
 
 class Crawler {
 
-    getHtmlByUrl(url) {
-        return new Promise((resolve, reject) => {
-            http.get(url, (res) => {
-                let resData = ''
+    async getHtmlByUrl(url) {
+        http.get(url, (res) => {
+            let resData = ''
 
-                res.on('data', chunk => {
-                    resData += chunk
-                })
-                res.on('end', () => {
-                    resolve(resData)
-                })
+            res.on('data', chunk => {
+                resData += chunk
             })
-        }) 
+            
+            await res.on('end', () => {
+                return resData
+            })
+
+            await res.on('error', (err) => {
+                return err
+            })
+        })
     }
 
-    findPages(homeUrl) {
+    findPages(obj) {
         let pageList = []
-        pageList.push(homeUrl)
+        pageList.push(obj.url)
 
-        return getHtmlByUrl(homeUrl).then( html => {
-            regs.defaultPages.test(html)
-            let pageCnt = RegExp.$1
-            if (pageCnt && pageCnt > 2) {
-                for (let i = 2; i < pageCnt; i++) {
-                    pageList.push(homeUrl + 'page/' + i)
-                }
+        let $ = cheerio.load(obj.html)
+        let totalPages = $('.next').attr('data-total-pages')
+        if (totalPages && totalPages > 1) {
+            for (let i = 2; i < pageCnt; i++) {
+                pageList.push(homeUrl + 'page/' + i)
             }
-            return pageList
+        }
+        return pageList
+    }
+
+    download(url) {
+        request.get(postUrl)
+            .on('error', (err) => {
+                console.info('request error:' + postUrl)
+                console.info(err)
+            })
+            .pipe(fs.createWriteStream())
+    }
+
+    downloadDefaultMainPage(obj) {
+        let pageList = findPages(obj)
+        let pagePostsUrl = []
+        pageList.map( pageUrl => {
+            getHtmlByUrl(pageUrl).then( html => {
+                let $ = cheerio.load(html)
+                $('.meta-item .post-date').each( (index, ele) => {
+                    let href = $(ele).attr('href')
+                    if (href) {
+                        pagePostsUrl.push(href)
+                    }
+                })
+            })
+        })
+        pagePostsUrl.map( postUrl => {
+            download(postUrl)
         })
     }
 
-    downloadDefaultMainPage(url) {
-        findPages(url).then( pageList => {
-            let pagePostsUrl = []
-            pageList.map( itemUrl => {
-                getHtmlByUrl(itemUrl).then( html => {
-                    let $ = cheerio.load(html)
-                    $('.meta-item .post-date').each( (index, ele) => {
-                        let href = $(ele).attr('href')
-                        if (href) {
-                            pagePostsUrl.push(href)
-                        }
-                    })
-                })
-            })
-            pagePostsUrl.map( postUrl => {
-                request.get(postUrl)
-                    .on('error', (err) => {
-                        console.info('request error:' + postUrl)
-                        console.info(err)
-                    })
-                    .pipe(fs.createWriteStream())
-            })
+    findPersonalPages(obj) {
+        let pageList = []
+        let archiveUrl = obj.url + 'archive'
+        pageList.push(archiveUrl)
+
+
+        
+    }
+
+    findNextPage(obj, pagelist) {
+        getHtmlByUrl(obj.url).then( html => {
+            let $ = cheerio.load(html)
+            let href = $('#next_page_link').attr('href')
+            if (/archivebefore_time=(.*?)/.test(href)) {
+                if (RegExp.$1) {
+                    pagelist.push(obj.url + 'archive' + RegExp.$1)
+                }
+            } else {
+                return pagelist
+            }
         })
+    }
+
+    downloadPersonalPage(obj) {
+        let pageList = findPersonalPages(obj)
+
     }
 
     run(url) {
@@ -66,9 +98,9 @@ class Crawler {
         } else { // main page
             getHtmlByUrl(url).then( html => {
                 if (regs.styleDefault.test(html)) { // default style page 
-                    downloadDefaultMainPage(url)
+                    downloadDefaultMainPage({ url, html })
                 } else { // personal style page
-                    downloadPersonalPage(url)
+                    downloadPersonalPage({ url, html })
                 }
             })
         }
